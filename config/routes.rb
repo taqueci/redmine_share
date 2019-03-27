93228bd1cacaa370dc96f3a13c19eb0b7fe91ffe
Takeshi Nakamura <taqueci.n@gmail.com>
2019-03-27 21:26:42 +0900

    Replace match with get/post in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RedmineApp::Application.routes.draw do
-  match '/link/dialog', to: 'link#dialog', via: [:get]
-  match '/link/autocomplete', to: 'link#autocomplete', via: [:get]
-  match '/link/share', to: 'link#share', via: [:post]
+  get '/link/dialog', to: 'link#dialog'
+  get '/link/autocomplete', to: 'link#autocomplete'
+  post '/link/share', to: 'link#share'
 end
